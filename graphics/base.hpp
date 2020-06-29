13159724ee5f9479b34abddf22e7097c8216333d
Tristan <code@thewoosh.me>
2020-06-29 14:22:09 +0200

    [Graphics] Initial Commit

---
+++ graphics/base.hpp
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * See the COPYING file for license information.
+ */
+
+#pragma once
+
+class GraphicsCoreBase {
+public:
+    virtual
+    ~GraphicsCoreBase() = default;
+
+    [[nodiscard]] virtual bool
+    CheckSupport() = 0;
+
+    [[nodiscard]] virtual bool
+    Setup() = 0;
+};
