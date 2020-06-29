9b848efff8bb889fa0de8433969cbb85de5f2a22
Tristan <code@thewoosh.me>
2020-06-29 15:00:41 +0200

    [Base] ApplicationInfo: Initial Commit

---
+++ base/application_info.hpp
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2020 Tristan. All Rights Reserved.
+ * See the COPYING file for license information.
+ */
+
+#pragma once
+
+#include "version.hpp"
+
+namespace ApplicationInfo {
+
+    static const char *applicationName = "AppName";
+    static constexpr Version applicationVersion{ 0, 1, 0 };
+
+    static const char *engineName = "EngineName";
+    static constexpr Version engineVersion{ 0, 1, 0 };
+
+} // namespace ApplicationInfo
